49038ee5ab9247170537a7e996d9e7cb0082d089
Guilherme <guilherme.viegas@tecnico.ulisboa.pt>
2020-10-30 15:30:44 +0000

    ransac done, cool inliers

--- Part 1 v2/homography2cameras.py
+++ Part 1 v2/homography2cameras.py
@@ -59,15 +59,15 @@ def get_biggest_contour(contours):
 
 
 #reading image
-pathfile = "images/paper_view.jpg"
-template_file = "images/template2.png"
+pathfile = "images/resized_paperview.jpg"
+template_file = "images/resized_template.png"
 
 img = cv2.imread(pathfile)
 template = cv2.imread(template_file)
 
 # img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 # template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
-
+'''
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 img_blur = cv2.GaussianBlur(img_gray, (5, 5), 1)
 img_threshold = cv2.Canny(img_blur, 100, 200)
@@ -76,10 +76,9 @@ template_gray = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
 template_blur = cv2.GaussianBlur(template_gray, (5, 5), 1)
 template = cv2.Canny(template_blur, 100, 200)
 
-'''
-cv2.imshow('image', img)
-cv2.imshow('template', template)
-'''
+#cv2.imshow('image', img)
+#cv2.imshow('template', template)
+
 img_contours = img.copy()
 img_biggest = img.copy()
 
@@ -109,9 +108,13 @@ cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 exit()
+'''
+
+#cv2.imshow('image', img)
+#cv2.imshow('template', template)
 
 #sift
-sift = cv2.xfeatures2d.SIFT_create()
+sift = cv2.SIFT_create()
 
 keypoints_1, descriptors_1 = sift.detectAndCompute(img, None)
 keypoints_2, descriptors_2 = sift.detectAndCompute(template,None)
@@ -122,7 +125,13 @@ for k in keypoints_1:
     x.append(k.pt[0])
     y.append(k.pt[1])
 
-print(keypoints_1[0])
+#print(keypoints_1[0])
+
+x_inv = []
+y_inv = []
+for k in keypoints_2:
+    x_inv.append(k.pt[0])
+    y_inv.append(k.pt[1])
 
 #feature matching
 bf = cv2.BFMatcher(cv2.NORM_L2, crossCheck=True)
@@ -130,33 +139,63 @@ bf = cv2.BFMatcher(cv2.NORM_L2, crossCheck=True)
 matches = bf.match(descriptors_1,descriptors_2)
 matches = sorted(matches, key = lambda x:x.distance)
 
-print(len(matches))
+#matches_inv = bf.match(descriptors_2, descriptors_1)
+#matches_inv = sorted(matches_inv, key = lambda x:x.distance)
+
+#print(len(matches))
 
 img3 = cv2.drawMatches(img, keypoints_1, template, keypoints_2, matches[:200], template, flags=2)
-plt.imshow(img3)
-'''
-plt.show()
+cv2.imshow('final', img3)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
+'''
+img_inv = cv2.drawMatches(template, keypoints_2, img, keypoints_1, matches_inv[:200], img, flags=2)
+cv2.imshow('final_inv', img_inv)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 '''
+plt.imshow(img3)
+x_final = []
+y_final = []
 
-x = np.array(x).reshape(-1,1)
-y = np.array(y).reshape(-1,1)
+'''
+print(matches[:200])
+for i in matches[:200]:
+    #print(i.trainIdx)
+    #x_final.append(keypoints_1[i.trainIdx].pt[0])
+    #y_final.append(keypoints_1[i.trainIdx].pt[1])
+    x_final.append(keypoints_1[i.trainIdx])
+    y_final.append(keypoints_2[i.trainIdx])'''
 
-reg = RANSACRegressor(random_state=0).fit(x, y)
-print(reg.score(x, y))
+'''for i in range(200):
+    if(keypoints_1[matches[])'''
+
+
+for k in matches[:200]:
+    x_final.append(keypoints_1[k.queryIdx].pt)
+    y_final.append(keypoints_2[k.trainIdx].pt)
+    
+
+x_final = np.array(x_final).reshape(-1,2)
+y_final = np.array(y_final).reshape(-1,2)
+
+
+reg = RANSACRegressor(random_state=0).fit(x_final, y_final)
+print(reg.score(x_final, y_final))
 
 inlier_mask = reg.inlier_mask_
 outlier_mask = np.logical_not(inlier_mask)
 
 print(len(inlier_mask))
-print(len(y))
+print(len(x_final))
+
+
 
 lw = 2
-plt.scatter(x[inlier_mask], y[inlier_mask], color='yellowgreen', marker='.',
+plt.scatter(x_final[inlier_mask][:,0], x_final[inlier_mask][:,1], color='yellowgreen', marker='.',
             label='Inliers')
-plt.scatter(x[outlier_mask], y[outlier_mask], color='gold', marker='.',
+plt.scatter(x_final[outlier_mask][:,0], x_final[outlier_mask][:,1], color='gold', marker='.',
             label='Outliers')
 
 plt.xlabel("Input")
