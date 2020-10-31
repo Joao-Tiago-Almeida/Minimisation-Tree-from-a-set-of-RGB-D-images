972b991ec54bc7512bf1815b4caf29a683910303
João Almeida <joao.marques.almeida@tecnico.ulisboa.pt>
2020-10-31 11:07:42 +0000

    review sort

--- Part 1 v2/homography2cameras.py
+++ Part 1 v2/homography2cameras.py
@@ -31,38 +31,47 @@ bf = cv2.BFMatcher(cv2.NORM_L2, crossCheck=True)
 
 matches = bf.match(descriptors_1,descriptors_2)
 
-xy = [[],[]]
+xy1 = []
+xy2 = []
 for k in matches:
-    xy[0].append(keypoints_1[k.queryIdx].pt)
-    xy[1].append(keypoints_2[k.trainIdx].pt)
-
+    xy1.append(keypoints_1[k.queryIdx].pt)
+    xy2.append(keypoints_2[k.trainIdx].pt)
+xy = zip(xy1,xy2)
 
 #  sort xy by the distance to center
-center = (img.shape[0]/2, img.shape[1]/2)
+center = (img.shape[1]/2, img.shape[0]/2)
 distance_points = lambda a: np.hypot( center[0] - a[0][0], center[1] - a[0][1] )
-xy.sort(key=distance_points)
+xy = sorted(xy, key=distance_points)
 
+xy1 = []
+xy2 = []
+for i in xy:
+    xy1.append(i[0])
+    xy2.append(i[1])
 
+xy = [xy1, xy2]
+breakpoint
 
 def ransac(xy,dim,tresh_num_inliers,tresh_dist_inliers):
 
     max_inliers = 0
+
+    video = 0   # video frame
+    template = 1    # template frame
+    x = 0   # xx coordinate
+    y = 1   # yy coordinate
     for i in range(len(xy[0])-3):
 
-        XY = ( xy[0][i:i+4], xy[1][i:i+4] ) # get four points
+        XY = ( xy[video][i:i+4], xy[template][i:i+4] ) # get four points
 
         # compute homography matrix -> A@H = B
         A = []
         B = []
 
-        video = 0   # video frame
-        template = 1    # template frame
-        x = 1   # xx coordinate
-        y = 0   # yy coordinate
         
         for elem in range(4):
 
-            point = (XY[0][elem],XY[1][elem])
+            point = (XY[video][elem],XY[template][elem])
 
             A.append( [ point[video][x], point[video][y], 1, 0, 0, 0, -point[video][x]*point[template][x], -point[video][y]*point[template][x] ] )
             A.append( [ 0, 0, 0, point[video][x], point[video][y], 1, -point[video][x]*point[template][y], -point[video][y]*point[template][y] ] )
@@ -88,7 +97,7 @@ def ransac(xy,dim,tresh_num_inliers,tresh_dist_inliers):
 
         max_inliers = ninliers if ninliers > max_inliers else max_inliers
 
-        if max_inliers > tresh_num_inliers*len(xy[0]): # if we get at least tresh_dist_inliers% of inlier points
+        if max_inliers > tresh_num_inliers*len(xy[video]): # if we get at least tresh_dist_inliers% of inlier points
             return H
 
     else:   # can´t find H
@@ -98,8 +107,8 @@ def ransac(xy,dim,tresh_num_inliers,tresh_dist_inliers):
 # the name is already speacified ...
 def getNumInliers( H, xy, dim, tresh_num_inliers ,tresh_dist_inliers, max_inliers ):
     
-    x = 1   # xx coordinate
-    y = 0   # yy coordinate
+    x = 0   # xx coordinate
+    y = 1   # yy coordinate
     inliers = 0
     for i in range(len(xy[0])):
         
@@ -113,13 +122,13 @@ def getNumInliers( H, xy, dim, tresh_num_inliers ,tresh_dist_inliers, max_inlier
         # normalize
         u, v = point_template_projective[0:2]/point_template_projective[2]
 
-        # if u < 0 : u = 0
-        # elif u > dim[1]: u = dim[1]
+        if u < 0 : u = 0
+        elif u > dim[1]: u = dim[1]
         
-        # if v < 0 : v = 0
-        # elif v > dim[0]: v = dim[0]
+        if v < 0 : v = 0
+        elif v > dim[0]: v = dim[0]
 
-        point_template_projective = np.array( [ v, u ] ) # it is treated (y, x)
+        point_template_projective = np.array( [ u, v ] ) # it is treated (y, x)
  
         distance_points = lambda x1, x2: np.hypot( x1[0] - x2[0], x1[1] - x2[1] )
         sse = distance_points(point_template_projective, point_template)
@@ -131,7 +140,7 @@ def getNumInliers( H, xy, dim, tresh_num_inliers ,tresh_dist_inliers, max_inlier
     return inliers
 
 
-final_H = ransac( xy=xy, dim=template.shape[0:2], tresh_num_inliers=0.25, tresh_dist_inliers = 10) 
+final_H = ransac( xy=xy, dim=template.shape[0:2], tresh_num_inliers=0.50, tresh_dist_inliers = 10) 
 
 breakpoint
 
