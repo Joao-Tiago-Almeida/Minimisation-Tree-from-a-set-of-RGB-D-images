3e3345b239842a564db66daaa71c7570683c3ef0
João Almeida <joao.marques.almeida@tecnico.ulisboa.pt>
2020-11-01 19:08:39 +0000

    projeto almeida

---
+++ Part 1 Almeida/homography2cameras.py
@@ -0,0 +1,168 @@
+import cv2
+import numpy as np
+from numpy.linalg import inv
+from random import randrange as rdr
+
+'''
+Manage to find homography that has at least {tresh_num_inliers} inliers.
+It is classified as inlier when its SSE is under than {tresh_dist_inliers} pixels.
+'''
+def getHomography(frame: np.array, template: np.array, tresh_num_inliers=0.50,
+                tresh_dist_inliers=5,max_iterations=1000, debug=False)  -> ( np.array ):
+
+    points = computeMatchPoints( frame, template)
+    homography = ransac( points ,template.shape[0:2], tresh_num_inliers,
+                            tresh_dist_inliers, max_iterations, debug) 
+    return homography
+
+'''
+sift = cv2.SIFT_create()
+'''
+def findFeatures( frame: np.array ) -> ( tuple ):
+    sift = cv2.SIFT_create()
+    keypoints, descriptors = sift.detectAndCompute( image = frame, mask = None )
+    return (keypoints, descriptors )
+
+'''
+Compute macth points
+'''
+def computeMatchPoints( frame: np.array, template: np.array ) -> ( list ):
+
+    keypoints_1, descriptors_1 = findFeatures( frame )
+    keypoints_2, descriptors_2 = findFeatures( template )
+
+    # Feature matching
+    bf = cv2.BFMatcher( normType = cv2.NORM_L2, # Euclidean norm
+                        crossCheck = True) # Returns only those matches with value (i,j) such that i-th descriptor in set A has j-th descriptor in set B as the best match and vice-versa
+    matches = bf.match( descriptors_1, descriptors_2)
+
+    # Append the match points between both images
+    points = [[],[]]
+    for k in matches:
+        points[0].append(keypoints_1[k.queryIdx].pt)  # queryIdx - Index of the descriptor in query descriptors
+        points[1].append(keypoints_2[k.trainIdx].pt)  # trainIdx - Index of the descriptor in train descriptors
+
+    return points
+
+'''
+Compute Homography
+'''
+def computeHomography( xy: list ) -> ( np.array ):
+
+    x = 0   # xx coordinate
+    y = 1   # yy coordinate
+    frame = 0   # video frame
+    template = 1    # template frame
+
+    # Compute homography matrix -> A@H = B
+    A = []
+    B = []
+
+    # Uses 4 points because the homograpgy has 8 degrees of freedom 
+    for i in range(4):
+
+        # Get random points
+        random_point = rdr( 0, len( xy[frame] ) )
+        random_point = (xy[frame][random_point],xy[template][random_point])
+        x_frame = random_point[frame][x]
+        y_frame = random_point[frame][y]
+        x_template = random_point[template][x]
+        y_template = random_point[template][y]
+
+        A.append( [ x_frame, y_frame, 1, 0, 0, 0, -x_frame*x_template, -y_frame*x_template ] )
+        A.append( [ 0, 0, 0, x_frame, y_frame, 1, -x_frame*y_template, -y_frame*y_template ] )
+
+        # coordinates at template frame 
+        B.append( x_template )
+        B.append( y_template )
+
+    A = np.array(A)
+    B = np.array(B)
+    H = None
+    
+    try: # in case of singular matrix
+
+        # Sum of Least Squares
+        H = inv(A.T@A)@(A.T@B)
+        H = np.append(H,1)  # appends H9 which was the normalization value
+        H = H.reshape(3,3)
+
+    except:
+        pass
+
+    return H
+
+'''
+Run Ransac Algoritm
+'''
+def ransac( xy, dim, tresh_num_inliers, tresh_dist_inliers, max_iterations, debug ) -> ( np.array ):
+
+    max_inliers = 0
+
+    # Run the algoritm {max_iterations} times
+    for i in range(max_iterations):
+
+        # get homography matrix
+        H = computeHomography( xy )
+        
+
+        try: num_inliers = getNumInliers( H, xy, dim, tresh_num_inliers ,tresh_dist_inliers )
+        except: pass    # Sometimes A is a singluar matrix and H can not be found
+
+        max_inliers, H = ( num_inliers, H)  if num_inliers > max_inliers else (max_inliers, H)
+
+        if max_inliers >= tresh_num_inliers*len(xy[0]): # if we get at least tresh_dist_inliers% of inlier points
+            if debug: print(f'\nAn Homography which fulfil all the requirements was found :)')
+            return H
+
+    else:   # can´t find H
+        percentage = max_inliers/len(xy[0])
+        if debug: print(f'\nFound a matrix H with {100*percentage:.1f}% inliers')
+        return [percentage, H]
+        
+'''
+Computes the number of inliers
+'''
+def getNumInliers( H, xy, dim, tresh_num_inliers ,tresh_dist_inliers ) -> ( int ):
+    
+    x = 0   # xx coordinate
+    y = 1   # yy coordinate
+    num_inliers = 0
+
+    distance_points = lambda x1, x2: np.hypot( x1[0] - x2[0], x1[1] - x2[1] )
+
+    for i in range(len(xy[0])):
+        
+        point_video = xy[0][i]
+        point_template = xy[1][i]
+
+        point_video_projective = np.array( [ point_video[x], point_video[y], 1 ] )
+
+        point_template_projective = H@point_video_projective
+
+        # Normalize
+        u, v = point_template_projective[0:2]/point_template_projective[2]
+
+        # Outside the template
+        if ( (u < 0) or (u >= dim[1]) or (v < 0) or (v >= dim[0]) ):
+            breakpoint
+            continue
+
+        point_template_projective = np.array( [ u, v ] )
+ 
+        # Geometric distance
+        sse = distance_points(point_template_projective, point_template)
+        # In/Out lier, decision
+        if sse < tresh_dist_inliers:
+            num_inliers += 1
+        
+    return num_inliers
+
+'''
+Generate a discret sequence
+'''
+def infinite_sequence() -> (int):
+    num = 0
+    while True:
+        yield num
+        num += 1
---
+++ Part 1 Almeida/part1.py
@@ -0,0 +1,155 @@
+'''
+Project ist190119
+PIV project part1 -
+inputs:
+
+template_image - string with the path of the template image
+
+moviein_filename - string with the input video
+
+movieout_filename - string with the output video
+
+return vars: None
+'''
+
+import cv2
+from homography2cameras import *
+
+DEBUG = True
+
+def part1(template_image: str, moviein_filename: str, movieout_filename: str) -> (None):
+
+    resize = lambda img, scale=2: cv2.resize(img, (int(img.shape[1]/scale) , int(img.shape[0]/scale) ))
+    
+    # Template properties
+    template = cv2.imread(template_image)
+    template = resize(template)
+
+    # Variables declaration
+    if DEBUG: gen = infinite_sequence()
+    homography = None
+    close_H = None
+    ratio_inliers = 0
+    frame_counter = 0
+
+    # Create a VideoCapture object and read from input file
+    cap = cv2.VideoCapture(moviein_filename)
+
+    # Check if camera opened successfully
+    if (cap.isOpened()== False): 
+        print("Error opening video stream or file")
+
+    # Read until video is completed
+    while(cap.isOpened()):
+        # Capture frame-by-frame
+        ret, img = cap.read()
+        if ret == True:
+            
+            # Actual frame number
+            if DEBUG: print(f'\rComputing homograpgy for frame {next(gen)+1}', end ='')
+
+            # Resize the image and compute an homography
+            img = resize(img)
+            if (frame_counter == 0): homography = getHomography( frame = img,
+                                                                 template = template,
+                                                                 tresh_num_inliers = 0.50,
+                                                                 tresh_dist_inliers = 1.5,
+                                                                 max_iterations = 1000,
+                                                                 debug = DEBUG)
+
+            # Get a list when the homography doesn't fulfil the requirements
+            if isinstance(homography,list):
+                ratio_inliers, close_H  = tuple(homography)  if ratio_inliers < homography[0] else (ratio_inliers, close_H)
+                frame_counter = 1
+                homography = None
+
+            # Wait 1 second (24 frames) when can not find a proper matrix
+            frame_counter = frame_counter+1 if (frame_counter<24 and frame_counter>0) else 0
+
+            # Exit when a finds a Homography that fulfil the requirements 
+            if homography is not None:
+                break
+            
+            # Press Q on keyboard to exit
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                break
+ 
+        # Break the loop
+        else: 
+            break
+
+    # When everything done, release the video capture object
+    cap.release()
+
+    # Storage the Homography with the best found 
+    homography = homography if homography is not None else close_H
+    
+    # Output dimension
+    height = template.shape[0]
+    width = template.shape[1]
+
+    # Auxiliar variables
+    total_frames = next(gen)
+    frame_number = 0
+
+    # Declare an empty frame to store all the frames
+    frames_sequence=[]
+
+    # Create a VideoCapture object and read from input file
+    cap = cv2.VideoCapture(moviein_filename)
+
+    # Check if camera opened successfully
+    if (cap.isOpened()== False): 
+        print("Error opening video stream or file")
+    
+     # Read until video is completed
+    while(cap.isOpened()):
+        # Capture frame-by-frame
+        ret, img = cap.read()
+        if ret == True:
+
+            # Actual frame number
+            if DEBUG:
+                frame_number+=1
+                print(f'\rComputing new prespective { "."*((frame_number%3)+1) }\t', end ='')
+
+            # Resize the image and compute an homography
+            img = resize(img)
+            frames_sequence.append(cv2.warpPerspective(img, homography, (width,height)))
+
+            # Press Q on keyboard to exit
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                break
+ 
+        # Break the loop
+        else: 
+            break
+
+    # When everything done, release the video capture object
+    cap.release()
+    print('')
+
+    # Define the codec and create VideoWriter object.The output is stored in movieout_filename.
+    # Define the fps to be equal to 24. Also frame size is passed.
+    
+    out = cv2.VideoWriter( filename = movieout_filename,
+                           fourcc = cv2.VideoWriter_fourcc(*'mp4v'),
+                           fps = 24.0, 
+                           frameSize = (frames_sequence[0].shape[1],frames_sequence[0].shape[0]),
+                           isColor = frames_sequence[0].shape[2] == 3 )
+    
+    # Read until video is completed
+    for i in range( len( frames_sequence ) ):
+
+        # Actual frame number
+        if DEBUG: print(f'\rWriting video {100*(i+1)/len(frames_sequence):.2f}%', end ='')
+
+        out.write(frames_sequence[i])
+
+    out.release()
+
+
+template_image = 'templates/templateP2.png'
+moviein_filename = 'videos/simpatico.mp4'
+movieout_filename = moviein_filename.replace(".mp4",".output.mp4")
+part1(template_image,moviein_filename,movieout_filename)
